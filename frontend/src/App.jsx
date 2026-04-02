65ebf5d1a126b57d6e28f5301895f843307b7be5
Manoel Vitor <brito.nascimento@escolar.ifrn.edu.br>
2026-04-02 09:37:01 -0300

    estrutura inicial fullstack

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex items-center w-full justify-center">
+      <Input />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <div className="mt-10">
+        <input type="text" placeholder="insira a tarefa" className="border p-2"/>
+    
+        <button className="p-2 border ml-3 cursor-pointer">Adicionar</button>
+    </div>
+  )
+}
+
+export default Input
